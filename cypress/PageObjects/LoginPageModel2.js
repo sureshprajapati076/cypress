aa3b52943a3808472eebca7d3ee4442c20d8a5cf
sureshprajapati076 <sureshprajapati076@gmail.com>
2023-01-26 21:11:31 -0800

    another way for pom model

--- cypress/PageObjects/LoginPageModel2.js
+++ cypress/PageObjects/LoginPageModel2.js
@@ -3,15 +3,23 @@
 
 class Login{
 
-    
+
     txtUserName = "input[placeholder='Username']"
     txtPassword = "input[placeholder='Password']"
     submitButton = "button[type='submit']"
 
 
+    //Even Another way
+    elements={
+        userName: ()=>cy.get("input[placeholder='Username']"),
+        userPass: ()=>cy.get("input[placeholder='Password']")
+    }
+
+
 
     setUserName(username){
-        cy.get(this.txtUserName).type(username)
+      //  cy.get(this.txtUserName).type(username)
+      this.elements.userName().type(username)
     }
 
     setPassword(password){
